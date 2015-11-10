6f7a75a5904dd20bd5324b3eddee66a24d09c2c2
jasonkit <jasonkit@gmail.com>
2015-11-10 10:18:30 +0800

    js formating

--- 3dcloth/js/scene_control.js
+++ 3dcloth/js/scene_control.js
@@ -3,81 +3,76 @@ var mat4 = require("gl-matrix").mat4;
 var vec3 = require("gl-matrix").vec3;
 var quat = require("gl-matrix").quat;
 
-function SceneControl(canvas)
-{
-    var self = this;
-    this.canvas = canvas;
-    this.isMouseDown = false;
-    this.startPos = {};
-    this.moved = false;
+function SceneControl(canvas) {
+  var self = this;
+  this.canvas = canvas;
+  this.isMouseDown = false;
+  this.startPos = {};
+  this.moved = false;
 
-    this.mouseMoveCallBack = null;
-    this.mouseUpCallBack = null;
-    this.mouseWheelCallBack = null;
-    this.clickCallBack = null;
+  this.mouseMoveCallBack = null;
+  this.mouseUpCallBack = null;
+  this.mouseWheelCallBack = null;
+  this.clickCallBack = null;
 
-    this.init = function(mmcb, mucb, mwcb, ccb) {
-        canvas.addEventListener("mousedown", this.onMouseDown);
-        canvas.addEventListener("wheel", this.onMouseWheel);
-        window.addEventListener("mouseup", this.onMouseUp);
-        window.addEventListener("mousemove", this.onMouseMove);
-        self.mouseMoveCallBack = mmcb;
-        self.mouseUpCallBack = mucb;
-        self.mouseWheelCallBack = mwcb;
-        self.clickCallBack = ccb;
-    };
+  this.init = function (mmcb, mucb, mwcb, ccb) {
+    canvas.addEventListener("mousedown", this.onMouseDown);
+    canvas.addEventListener("wheel", this.onMouseWheel);
+    window.addEventListener("mouseup", this.onMouseUp);
+    window.addEventListener("mousemove", this.onMouseMove);
+    self.mouseMoveCallBack = mmcb;
+    self.mouseUpCallBack = mucb;
+    self.mouseWheelCallBack = mwcb;
+    self.clickCallBack = ccb;
+  };
 
-    this.onMouseDown = function(e)
-    {
-        self.isMouseDown = true; 
-        self.startPos.x = e.clientX;
-        self.startPos.y = e.clientY;
-        self.moved = false;
-    };
-    
-    this.onMouseUp = function(e)
-    {
-        if (self.isMouseDown) {
-            self.isMouseDown = false; 
-            
-            if (typeof self.mouseMoveCallBack === "function") {
-                self.mouseUpCallBack();
-            }
+  this.onMouseDown = function (e) {
+    self.isMouseDown = true;
+    self.startPos.x = e.clientX;
+    self.startPos.y = e.clientY;
+    self.moved = false;
+  };
 
-            if (!self.moved) {
-                self.clickCallBack();
-            }
-        }    
-    };
+  this.onMouseUp = function (e) {
+    if (self.isMouseDown) {
+      self.isMouseDown = false;
 
-    this.onMouseMove = function(e)
-    {
-        if (!self.isMouseDown) return;
-        
-        self.moved = true;
-        
-        var q = quat.create();
-        var axis = [self.startPos.y-e.clientY, e.clientX-self.startPos.x,0];
-        var rad = vec3.length(axis)/200.0;
-        vec3.normalize(axis, axis);
-        quat.setAxisAngle(q, axis, rad);
+      if (typeof self.mouseMoveCallBack === "function") {
+        self.mouseUpCallBack();
+      }
 
-        if (typeof self.mouseMoveCallBack === "function") {
-            self.mouseMoveCallBack(q);
-        }
-        
-        e.preventDefault();
-        e.stopPropagation();
-    };
+      if (!self.moved) {
+        self.clickCallBack();
+      }
+    }
+  };
 
-    this.onMouseWheel = function(e)
-    {
-        if (typeof self.mouseMoveCallBack === "function") {
-            self.mouseWheelCallBack(vec3.fromValues(0,0,e.deltaY));
-        }
-        e.preventDefault();
-        e.stopPropagation();
-    };
+  this.onMouseMove = function (e) {
+    if (!self.isMouseDown) return;
+
+    self.moved = true;
+
+    var q = quat.create();
+    var axis = [self.startPos.y - e.clientY, e.clientX - self.startPos.x, 0];
+    var rad = vec3.length(axis) / 200.0;
+    vec3.normalize(axis, axis);
+    quat.setAxisAngle(q, axis, rad);
+
+    if (typeof self.mouseMoveCallBack === "function") {
+      self.mouseMoveCallBack(q);
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  this.onMouseWheel = function (e) {
+    if (typeof self.mouseMoveCallBack === "function") {
+      self.mouseWheelCallBack(vec3.fromValues(0, 0, e.deltaY));
+    }
+    e.preventDefault();
+    e.stopPropagation();
+  };
 
 }
 
